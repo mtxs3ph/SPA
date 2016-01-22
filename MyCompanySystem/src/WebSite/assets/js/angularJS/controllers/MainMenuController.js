112c65ac513204b4058af57d7cf9ee43c0b724be
mtxs3ph <diego.siebra@gmail.com>
2016-01-22 10:25:34 -0200

    Base Version

---
+++ MyCompanySystem/src/WebSite/assets/js/angularJS/controllers/MainMenuController.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('MainMenuModule')
+        .controller('MainMenuController', controller);
+
+    controller.$inject = ['$location']; 
+
+    function controller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'MainMenuModule';
+
+        activate();
+
+        function activate() { }
+    }
+})();
