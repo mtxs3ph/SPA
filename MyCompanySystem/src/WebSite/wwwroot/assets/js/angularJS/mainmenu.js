ca03f6acfe9db353bda98c64df3e7946a1ef15dd
mtxs3ph <diego.siebra@gmail.com>
2016-01-22 11:23:09 -0200

    Reorganização

---
+++ MyCompanySystem/src/WebSite/wwwroot/assets/js/angularJS/mainmenu.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+    var app = angular.module('main-menu-directives',[]);
+    
+    app.directive("mainMenu", function () {
+        return { 
+            retrict: 'E',
+            tempalteURL: "main-menu.html"
+        };
+    });
+    
+})();
